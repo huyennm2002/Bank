29b92680d7469fd2318481f834c1191e352f25d6
Raven_Hedgie <82295811+Ravend2801@users.noreply.github.com>
2023-04-25 11:37:16 -0500

    update

--- src/backend/MakeTransaction.php
+++ src/backend/MakeTransaction.php
@@ -8,12 +8,23 @@
 </head>
 
 <body>
- 
   <?php
-/*    $mysqli = new mysqli("mysql.eecs.ku.edu", "m449n496", "mae9AhH3", "m449n496");
+    ini_ set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    session_start();
+    $mysqli = new mysqli("mysql.eecs.ku.edu", "m449n496", "mae9AhH3", "m449n496");
+    if ( mysqli_connect_errno() ) {
+      printf("Connect failed: %s\n", $mysqli->connect_error);
+      exit();
+    }
     $note = $_POST['Note'];
     $amount = $_POST['Amount'];
     $transactiondate = $_POST['Date'];
+    $sender_id = $_SESSION['Sender-id'];
+    $receiver_id = $_SESSION['Receiver-id']; 
+    $acc_id = $_POST['account-id'];
+   
 
     if($note == '' || $amount == ''|| $transactiondate == '') {
       error("Please enter all required fields");
@@ -22,110 +33,38 @@
         printf("Connect failed: %s\n", $mysqli->connect_error);
         exit();
       } else{
-        $sql = "INSERT INTO TRANSACTIONS (Note, Amount, TransactionDate) VALUES ('$note', '$amount', '$transactiondate');";
-        if ($mysqli->query($sql) === TRUE) {
-          echo "<p>Transaction was successfully created</p>";
-        }
-      $mysqli->close();
+      $query = $mysqli -> prepare("SELECT amount(*) FROM Users WHERE id = '$acc_id'");
+      $query = "UPDATE Accounts SET balance = balance - $amount WHERE ID = '$sender_id'";
+      $create_query = "INSERT INTO Actions (AccountID, Amount, Note, TransactionDate) values ('$sender_id', '$amount', '$note', '$transactiondate','Transaction', NOW())";
+      if ($mysqli->query($sql) === TRUE && $mysqli->query($create_query)) {
+        echo "<p>The amount has been transfered successfully</p>";
+      } else {
+        echo "<p>Error, cannot make transaction</p>";
       }
-    
-	}
-*/
+      $query = "UPDATE Accounts SET balance = balance + $amount WHERE ID = '$receiver_id'"; 
+      $create_query = "INSERT INTO Actions (AccountID, Amount, Note, TransactionDate) values ('$sender_id', '$amount', '$note', '$transactiondate','Transaction', NOW())";
+      $mysqli->close();
+    }
+  }
 
-
-/*  function error($msg) {
+  function error($msg) {
   ?>
-      <form method="GET" action="/src/frontend/Transactions.html">
-        <div class="container">
-          <span>Error: <?= $msg ?></span>
-          <button class="backbtn" type="submits">Try again!</button>
-        </div>
-      </form>
-    <?php
-  } 
-*/
-
-
-class makeTransaction {
-
-    /**
-     * Open the database connection
-     */
-    public function __construct() {
-        // open database connection
-        $mysqli = new mysqli("mysql.eecs.ku.edu", "m449n496", "mae9AhH3", "m449n496");
-    }
-
-    /**
-     * PDO instance
-     * @var PDO 
-     */
-    private $pdo = null;
-
-    /**
-     * Transfer money between two accounts
-     * @param int $from
-     * @param int $to
-     * @param float $amount
-     * @return true on success or false on failure.
-     */
-    public function transfer($from, $to, $amount) {
-
-        try {
-            $this->pdo->beginTransaction();
-
-            // get available amount of the transferer account
-            $sql = 'SELECT amount FROM accounts WHERE id=:from';
-            $stmt = $this->pdo->prepare($sql);
-            $stmt->execute(array(":from" => $from));
-            $availableAmount = (int) $stmt->fetchColumn();
-            $stmt->closeCursor();
-
-            if ($availableAmount < $amount) {
-                echo 'Insufficient amount to transfer';
-                return false;
-            }
-            // deduct from the transferred account
-            $sql_update_from = 'UPDATE accounts
-				SET amount = amount - :amount
-				WHERE id = :from';
-            $stmt = $this->pdo->prepare($sql_update_from);
-            $stmt->execute(array(":from" => $from, ":amount" => $amount));
-            $stmt->closeCursor();
-
-            // add to the receiving account
-            $sql_update_to = 'UPDATE accounts
-                                SET amount = amount + :amount
-                                WHERE id = :to';
-            $stmt = $this->pdo->prepare($sql_update_to);
-            $stmt->execute(array(":to" => $to, ":amount" => $amount));
-
-            // commit the transaction
-            $this->pdo->commit();
-
-            echo 'The amount has been transferred successfully';
-
-            return true;
-        } catch (PDOException $e) {
-            $this->pdo->rollBack();
-            die($e->getMessage());
-        }
-    }
-
-    /**
-     * close the database connection
-     */
-    public function __destruct() {
-        // close the database connection
-        $this->pdo = null;
-    }
-
-}
-
-
+        <form method="GET" action="/src/frontend/Transaction.html">
+          <div class="container">
+            <span>Error: <?= $msg ?></span>
+            <button class="backbtn" type="submits">Try again!</button>
+          </div>
+        </form>
+      <?php
+    } 
   ?>
+
+<?php
+
+?>
 
 </body>
 
 </html>
+
 
